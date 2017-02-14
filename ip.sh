3a236ebd3fffaa39d92cf3cd67d5a02bd54cd402
Your Name <ahoj@ahoj.cz>
2017-02-14 10:03:17 +0100

    V 1.3 utery, ip.sh - systémové informace

---
+++ ip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cas=`date +%T`
+echo "Aktuální čas:" $cas
+
+ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
+echo "Moje IP" $ip
+
+rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
+echo "Přijatá data:" $rx
+
+tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print$3}' | awk -F ")" '{print$1}'`
+echo "Odeslaná data:" $tx
